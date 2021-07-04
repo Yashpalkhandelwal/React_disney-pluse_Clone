60a7dfbe08f37fbba693aaa169cd525f3df5de74
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2021-07-04 17:25:30 +0530

    home background done

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+    return (
+        <Container>
+            home
+        </Container>
+    )
+}
+
+export default Home
+
+
+const Container = styled.main `
+    min-height:100vh;
+    padding: 0 calc(3.5vw + 5px);
+   
+`
