614de174a74a048e8a6e0df929a82fe2953a7e56
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2021-07-04 22:14:58 +0530

    slider_slick added

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styled from 'styled-components'
-
-
+import ImageSilder from './ImageSlider'
 
 function Home() {
     return (
         <Container>
-            home
+            <ImageSilder/>
         </Container>
 
     )
@@ -17,8 +16,21 @@ export default Home
 
 
 const Container = styled.main `
-    min-height:100vh;
+    min-height: calc(100vh - 70px);
     padding: 0 calc(3.5vw + 5px);
+    position:relative;
+
+    &:before{        
+        background: url("/images/home-background.png") center center / cover 
+        no-repeat fixed;
+        content: "";
+        position: absolute;
+        top: 0;
+        left : 0;
+        right:0;
+        bottom:0;
+        z-index:-1;
+    }
    
 `
 
