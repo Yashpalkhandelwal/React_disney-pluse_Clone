8b6ed64db2a1615050be063ff413e162cdf5bc8a
Yashpalkhandelwal <yashkumar1235pal@gmail.com>
2021-07-04 14:22:43 +0530

    nav-bar-with-background-color

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,19 @@
 import React from 'react'
 
+import styled from 'styled-components'
+
 function Header() {
     return (
-        <div>
+        <Container>
             Hello
-        </div>
+        </Container>
     )
 }
 
 export default Header
+
+
+const Container = styled.div `
+    height:70px;
+    background:#090b13;
+`
